689b2ea858e7de4784f0eb038395c57d70bb85a6
Thapelo <folotithapelo@gmail.com>
2025-08-08 21:15:51 +0200

    created and updated App.jsx and Main.jsx

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>GitHub User Search</h1>
+      <p>This is the basic setup. Start searching users!</p>
+    </div>
+  );
+}
+
+export default App;
